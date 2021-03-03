38e9a3e475027a7b8f79230c7b28b0f5ed3ae95e
gmartpad <gabrielmpwork@gmail.com>
2021-03-03 08:19:59 -0300

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import './assets/css/base/base.css'
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 
 
 function App() {
 
-  const Router = () => {
-    const pathname = window.location.pathname;
-
-    if(pathname === "/sobre"){
-      return <Sobre/>
-    }else{
-      return <Home/>
-    }
-  }
-
   return (
   <>
-    { Router() }
+    <BrowserRouter>
+      <Route exact path="/">
+        <Home/>
+      </Route> 
+      <Route path="/sobre">
+        <Sobre/>
+      </Route>
+    </BrowserRouter>
   </>
   )
 }
