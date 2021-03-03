a89d872c15d4011907fc3440d3046ae23956bafa
gmartpad <gabrielmpwork@gmail.com>
2021-03-03 07:52:18 -0300

    feat: inicio do problema

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+
 
 function App() {
+
+  const Router = () => {
+    const pathname = window.location.pathname;
+
+    if(pathname === "/sobre"){
+      return <Sobre/>
+    }else{
+      return <Home/>
+    }
+  }
+
   return (
   <>
-
+    { Router() }
   </>
   )
 }
