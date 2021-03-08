79a520d47d7636812dd4cd9be45e5dd1f700c48f
gmartpad <gabrielmpwork@gmail.com>
2021-03-08 02:12:38 -0300

    feat: conexao com api

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+export const busca = async (url) => {
+    const resultado = await api.get(url)
+    setDado(resultado.data)
+}
