b631790223821d81eb3a87c762336d3f6beedf29
gmartpad <gabrielmpwork@gmail.com>
2021-03-09 00:18:11 -0300

    feat: exibindo post

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -8,12 +8,12 @@ const ListaPost = ( { url } ) => {
 
     useEffect(()=>{
         busca(url, setPosts)
-    }, [])
+    }, [url])
 
     return (
         <section className="posts container">
             {posts.map((i,k) =>
-                <Link key={k} className={`cartao-post cartao-post--${i.categoria}`} >
+                <Link key={k} className={`cartao-post cartao-post--${i.categoria}`} to={`/posts/${i.id}`} >
                     <article key={i.id}>
                         <h3 className="cartao-post__titulo" >
                             {i.title}
