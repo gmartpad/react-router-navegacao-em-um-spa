69e4d65504bfd47999806b7d8352c643cf009dee
gmartpad <gabrielmpwork@gmail.com>
2021-03-09 23:42:25 -0300

    feat: post inexistente

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { busca } from '../api/api'
 import '../assets/css/post.css'
 
@@ -7,12 +7,17 @@ import '../assets/css/post.css'
 
 const Post = () => {
 
+    let history = useHistory()
+
     const { id } = useParams();
 
     const[post, setPost] = useState({})
 
     useEffect(()=>{
         busca(`/posts/${id}`, setPost)
+        .catch(()=>{
+            history.push('/404');
+        })
     }, [id])
 
     return(
