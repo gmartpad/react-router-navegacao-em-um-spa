d8d80059e73e7e199a855b0e04c6d2294f3679b5
gmartpad <gabrielmpwork@gmail.com>
2021-03-10 20:32:43 -0300

    feat: criando subcategoria

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, Route, useRouteMatch } from 'react-router-dom'
 import ListaCategorias from '../components/ListaCategorias'
 import ListaPost from '../components/ListaPost'
+import { busca } from '../api/api' 
 import '../assets/css/blog.css'
 
 const Categoria = () => {
     const { id } = useParams()
     const { path } = useRouteMatch()
+    const [subcategorias, setSubcategorias] = useState([])
+
+    useEffect(()=>{
+        busca(`/categorias/${id}`, (categoria) => {
+            setSubcategorias(categoria.subcategorias)
+        })
+    }, [id])
+
     return(
         <>
             <div className="container">
